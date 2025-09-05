b74ac50816dd0e375d02916eaf35a0000a42a665
PFKSagem <tecphi@gmail.com>
2025-09-05 16:51:52 -0300

    teste

--- hahaha.py
+++ hahaha.py
@@ -17,7 +17,7 @@ import subprocess
 #  "rsync -av --progress cts@192.168.15.201:/opt/android/atv14/cts/android-cts/results/latest/compatibility_result.css /var/www/html/test/"
 #  "rsync -av --progress cts@192.168.15.201:/opt/android/atv14/cts/android-cts/results/latest/logo.png /var/www/html/test/"
 
-vph1path1 = "/opt/android/atv14/cts/android-cts/results/latest/test_result.html /var/www/html/test/"
+vph1path1 = "/opt/android/atv14/cts/android-cts/results/latest/test_result.html"
 # vpathsource2 = "/opt/android/atv14/cts/android-cts/results/latest/test_result_failures_suite.html /var/www/html/test/"
 # vpathsource3 = "/opt/android/atv14/cts/android-cts/results/latest/compatibility_result.css /var/www/html/test/"
 # vpathsuorce4 = "/opt/android/atv14/cts/android-cts/results/latest/logo.png /var/www/html/test/"
@@ -28,7 +28,7 @@ vvmpath1 = "/opt"
 # vpathdest3 = "/var/www/html/test/"
 # vpathdest4 = "/var/www/html/test/"
 
-vcommand = "/usr/bin/rsync -av"
+# vcommand = "/usr/bin/rsync -av"
 
 # server ph3
 vipsource = "192.168.15.207"
@@ -39,24 +39,27 @@ varrouba = "@"
 # server vm
 vipdest = "192.168.15.156"
 
-command = [vcommand, vuser, varrouba, vipsource, vdoublepoint, vph1path1, vvmpath1]
+# command = [vcommand, vuser, varrouba, vipsource, vdoublepoint, vph1path1, vvmpath1]
+command = ['rsync', '-avz', vuser, varrouba, vipsource, vdoublepoint, vph1path1, vvmpath1]
 
-try:
-    result = subprocess.run(
-        command,
-        check=True,  # Raises an exception if the command fails
-        capture_output=True, # Captures stdout and stderr
-        text=True # Decodes output as text
-    )
-    print("rsync completed successfully.")
-    print("Stdout:", result.stdout)
-    print("Stderr:", result.stderr)
+print(command)
 
-except subprocess.CalledProcessError as e:
-    print("rsync failed.")
-    print("Return code:", e.returncode)
-    print("Stdout:", e.stdout)
-    print("Stderr:", e.stderr)
+# try:
+#     result = subprocess.run(
+#         command,
+#         check=True,  # Raises an exception if the command fails
+#         capture_output=True, # Captures stdout and stderr
+#         text=True # Decodes output as text
+#     )
+#     print("rsync completed successfully.")
+#     print("Stdout:", result.stdout)
+#     print("Stderr:", result.stderr)
+
+# except subprocess.CalledProcessError as e:
+#     print("rsync failed.")
+#     print("Return code:", e.returncode)
+#     print("Stdout:", e.stdout)
+#     print("Stderr:", e.stderr)
 
 
 
