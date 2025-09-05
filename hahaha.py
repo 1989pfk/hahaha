efaeb73b3d86bb6bb0337b54ed15ca10e1777880
Philipp Fregolão <tecphi@gmail.com>
2025-09-05 07:32:03 -0300

    Ballons added

--- hahaha.py
+++ hahaha.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import time
 
-st.markdown("Markdown")
+st.title("Progess")
+
+# Create a progress bar with an initial value of 0%
+progress_bar = st.progress(0)
+
+# Simulate a task with a loop
+for i in range(100):
+    # Update the progress bar's value
+    progress_bar.progress(i + 1)
+    time.sleep(0.01)
+
+st.success('Task complete!')    
+
+st.balloons()
